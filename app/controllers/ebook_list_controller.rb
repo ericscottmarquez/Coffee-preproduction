3b15c8fee9b6faa5bf402c0e6d294b303b51263b
ericscottmarquez <ericscottmarquez@gmail.com>
2018-09-30 13:18:29 -0600

    ebook list controller bug

--- app/controllers/ebook_list_controller.rb
+++ app/controllers/ebook_list_controller.rb
@@ -1,15 +1,16 @@
 class EbookListController < ApplicationController
 
-def create
+  def create
 
-	@ebook_list = ebook_list.new(params[:ebook_list])
+  	@ebook_list = ebook_list.new(params[:ebook_list])
 
-	respond_to do |format|
-		if @ebook_list.save
-			Ebook_ListMailer.with(ebook_list: @ebook_list).welcome_email.deliver_now
+  	respond_to do |format|
+  		if @ebook_list.save
+  			Ebook_ListMailer.with(ebook_list: @ebook_list).welcome_email.deliver_now
 
-			format.html { redirect_to(:index, flash: "THANKS FOR SIGNING UP!")}
-		end
-	end
+  			format.html { redirect_to(:index, flash: "THANKS FOR SIGNING UP!")}
+  		end
+  	end
+  end
+
 end
-
