8a43ff33f98a29e61d81de22b6284ca6ad85238c
ericscottmarquez <ericscottmarquez@gmail.com>
2018-10-12 11:07:56 -0600

    trying to get this working updated yarn and took off devise reroute

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,4 @@ class ApplicationController < ActionController::Base
 	
 	include PublicActivity::StoreController
 
-  def after_sign_in_path_for(resource)
-    new_user_registration_path
-  end
-
 end
