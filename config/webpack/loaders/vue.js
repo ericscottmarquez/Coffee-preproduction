d252afb0bf999bc791ec9b7ca74e87c0c4aae792
ericscottmarquez <ericscottmarquez@gmail.com>
2018-10-12 21:08:13 -0600

    changing vue environment

--- config/webpack/loaders/vue.js
+++ config/webpack/loaders/vue.js
@@ -5,9 +5,5 @@ const inDevServer = process.argv.find(v => v.includes('webpack-dev-server'))
 const extractCSS = !(inDevServer && (devServer && devServer.hmr)) || isProduction
 
 module.exports = {
-  test: /\.vue(\.erb)?$/,
-  use: [{
-    loader: 'vue-loader',
-    options: { extractCSS }
-  }]
+  mode: 'production'
 }
