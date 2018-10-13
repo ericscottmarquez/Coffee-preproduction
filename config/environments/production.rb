d99dcb1ce5af2b35655eeeb2a3e56b064c7e72a4
ericscottmarquez <ericscottmarquez@gmail.com>
2018-10-12 22:30:54 -0600

    production serve public assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,8 @@ Rails.application.configure do
   # Verifies that versions and hashed value of the package contents in the project's package.json
   config.webpacker.check_yarn_integrity = false
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.public_file_server.enabled configures = true
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
