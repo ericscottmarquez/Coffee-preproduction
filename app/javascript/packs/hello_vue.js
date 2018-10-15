0bf31a806a7d3f96cf962afb71020b24d49d14e3
ericscottmarquez <ericscottmarquez@gmail.com>
2018-10-15 02:42:43 -0600

    dom rendering still broken in production

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const el = document.body.appendChild(document.createElement('hello'))
 const app = new Vue({
     el: domElement,
-    render: h => h(RootComponent)
+    render: h => h(App)
   })
 
   console.log(app)
