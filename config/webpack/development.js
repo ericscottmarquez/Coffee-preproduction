07677de6f8f534b969bd39ccce7b2c2624dfb5f1
ericscottmarquez <ericscottmarquez@gmail.com>
2018-10-14 21:59:47 -0600

    okay trying to edit vueJS production mode for the dom to mount correctly

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,3 +2,14 @@ const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
 
+var webpack = require('webpack')
+
+module.exports = {
+  // ...
+  plugins: [
+    // ...
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ]
+}
