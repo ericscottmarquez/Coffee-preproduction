7c83d4c78f62dd84f4bf2050100f1624c351e60b
ericscottmarquez <ericscottmarquez@gmail.com>
2018-10-14 20:10:24 -0600

    still trying here to get vue into production mode

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,17 +2,5 @@ const environment = require('./environment')
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
-module.exports = {
-  resolve: {
-    alias: {
-      jquery: 'jquery/src/jquery',
-      vue: 'vue/dist/vue.js',
-      React: 'react',
-      ReactDOM: 'react-dom',
-      vue_resource: 'vue-resource/dist/vue-resource',
-    }
-  }
-}
-
 module.exports = environment.toWebpackConfig()
 
