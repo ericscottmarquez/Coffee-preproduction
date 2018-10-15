7d6f026454e914c377df4172e7d17d359e02e214
ericscottmarquez <ericscottmarquez@gmail.com>
2018-10-14 22:22:18 -0600

    uhhh

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,12 +2,3 @@ const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
 
-var webpack = require('webpack')
-
-module.exports = {
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    })
-  ]
-}
